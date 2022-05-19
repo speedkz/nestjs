71cf83e90320fcd2a9750520e152d18cd243ea03
Bui Tuan Dung (TECHVIFY.G2) <dungbt@techvify.com.vn>
2022-05-19 17:41:44 +0700

    init commit

---
+++ src/app.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthService } from './auth/auth.service';
+import { LocalAuthGuard } from './auth/local-auth.guard';
+
+@Controller()
+export class AppController {
+  constructor(
+    private readonly appService: AppService,
+    private authService: AuthService,
+  ) {}
+
+  @UseGuards(LocalAuthGuard)
+  @Post('auth/login')
+  async login(@Request() req) {
+    return this.authService.login(req.user);
+  }
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+}
---
+++ src/auth/auth.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from 'src/users/users.module';
+import { AuthService } from './auth.service';
+import { PassportModule } from '@nestjs/passport';
+import { LocalStrategy } from './local.stragety';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constant';
+
+@Module({
+  imports: [
+    UsersModule,
+    PassportModule,
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
+  providers: [AuthService, LocalStrategy],
+  exports: [AuthService],
+})
+export class AuthModule {}
